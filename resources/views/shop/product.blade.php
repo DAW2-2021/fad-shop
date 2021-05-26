49a8b46571984d1c465d3723bf3632760fa6e5f0
AitorRT <aitorruto@gmail.com>
2021-05-26 16:07:46 +0200

    start rating

--- resources/views/shop/product.blade.php
+++ resources/views/shop/product.blade.php
@@ -4,6 +4,58 @@
 @endsection
 
 @section('content')
+    <style>
+        @import url(https://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
+        @import url(http://fonts.googleapis.com/css?family=Calibri:400,300,700);
+
+
+
+        .rating {
+            border: none;
+            margin-right: 49px
+        }
+
+        .myratings {
+            font-size: 85px;
+            color: green
+        }
+
+        .rating>[id^="star"] {
+            display: none
+        }
+
+        .rating>label:before {
+            margin: 5px;
+            font-size: 2.25em;
+            font-family: FontAwesome;
+            display: inline-block;
+            content: "\f005"
+        }
+
+        .rating>.half:before {
+            content: "\f089";
+            position: absolute
+        }
+
+        .rating>label {
+            color: #ddd;
+            float: right
+        }
+
+        .rating>[id^="star"]:checked~label,
+        .rating:not(:checked)>label:hover,
+        .rating:not(:checked)>label:hover~label {
+            color: #FFD700
+        }
+
+        .rating>[id^="star"]:checked+label:hover,
+        .rating>[id^="star"]:checked~label:hover,
+        .rating>label:hover~[id^="star"]:checked~label,
+        .rating>[id^="star"]:checked~label:hover~label {
+            color: #FFED85
+        }
+
+    </style>
     @include('includes.navbar-shop')
     <!-- PRODUCT -->
     <div class="container-sm container-fluid mb-3 px-4">
@@ -209,25 +261,53 @@
                     <h5 class="modal-title" id="exampleModalLabel">Añade una review</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                    <span class="fa fa-star"></span>
-                    <br />
-                    <label for="" class="form-label mt-2">Opinión</label>
-                    <input type="text" class="form-control" />
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary align-self-start">
-                        Enviar opinión
-                    </button>
-                </div>
+                <form action="#" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <h4 class="mt-1">Puntuación</h4>
+                    <fieldset class="rating"> <input type="radio" required id="star5" name="rating" value="10" /><label
+                            class="full" for="star5" title="Awesome - 5 stars"></label> <input type="radio" id="star4half"
+                            name="rating" value="9" /><label class="half" for="star4half"
+                            title="Pretty good - 4.5 stars"></label> <input type="radio" id="star4" name="rating"
+                            value="8" /><label class="full" for="star4" title="Pretty good - 4 stars"></label> <input
+                            type="radio" id="star3half" name="rating" value="7" /><label class="half" for="star3half"
+                            title="Meh - 3.5 stars"></label> <input type="radio" id="star3" name="rating" value="6" /><label
+                            class="full" for="star3" title="Meh - 3 stars"></label> <input type="radio" id="star2half"
+                            name="rating" value="5" /><label class="half" for="star2half"
+                            title="Kinda bad - 2.5 stars"></label>
+                        <input type="radio" id="star2" name="rating" value="4" /><label class="full" for="star2"
+                            title="Kinda bad - 2 stars"></label> <input type="radio" id="star1half" name="rating"
+                            value="3" /><label class="half" for="star1half" title="Meh - 1.5 stars"></label> <input
+                            type="radio" id="star1" name="rating" value="2" /><label class="full" for="star1"
+                            title="Sucks big time - 1 star"></label> <input type="radio" id="starhalf" name="rating"
+                            value="1" /><label class="half" for="starhalf" title="Sucks big time - 0.5 stars"></label>
+                    </fieldset>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary align-self-start">
+                            Enviar opinión
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 @endsection
 @section('extraFooter')
+    <script>
+        $(document).ready(function() {
 
+            $("input[type='radio']").click(function() {
+                var sim = $("input[type='radio']:checked").val();
+                //alert(sim);
+                if (sim < 3) {
+                    $('.myratings').css('color', 'red');
+                    $(".myratings").text(sim);
+                } else {
+                    $('.myratings').css('color', 'green');
+                    $(".myratings").text(sim);
+                }
+            });
+        });
+
+    </script>
 @endsection
