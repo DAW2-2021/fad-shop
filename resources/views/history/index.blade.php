61fe2f0398363f843c7b2e24a98e601a132ae0a7
FranBF <49651005+FranBF@users.noreply.github.com>
2021-05-28 20:00:26 +0200

    history added

---
+++ resources/views/history/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+@section('extraHeader')
+
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="p-3 pb-md-4 mx-auto text-center">
+            <h1 class="display-4 fw-normal">Productos</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="profile-head">
+                    <div class="row">
+                        <div class="col-md-11">
+                            <h5>
+                                Todos los productos: </h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Nº Prod.</th>
+                        <th scope="col">Creador/Modificador</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Precio</th>
+                        <th scope="col">Stock</th>
+                        <th scope="col">Accion</th>
+                        <th scope="col">Fecha creación</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($prod_history as $product)
+                        <tr>
+                            <th scope="row">{{ $product->id }}</th>
+                            <td>{{ $product->shop->user->username }}</td>
+                            <td>{{ $product->name }}</td>
+                            @if (Str::length($product->description) <= 20)
+                                <td>{{ $product->description }}</td>
+                            @else
+                                <td>{{ Str::substr($product->description, 0, 20) . '...' }}</td>
+                            @endif
+                            <td>{{ $product->price }}</td>
+                            <td>{{ $product->stock }}</td>
+                            <td>{{ $product->action }}</td>
+                            <td>{{ $product->created_at }}</td>
+                            @if ($product->created_at != $product->updated_at)
+                                <td>{{ $product->updated_at }}</td>
+                            @else
+                                <td>&nbsp;</td>
+                            @endif
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="row d-flex justify-content-center paginator">{{ $prod_history->links() }}</div>
+    </div>
+@endsection
+@section('extraFooter')
+
+@endsection
